8ba07ab684401c3e1d5fe8d1b3d9e277ec96c79e
Niwgoat <nimafathinimafathi@gmail.com>
2018-10-25 19:19:44 +0330

    Block and Gildoony updated

--- src/Elements/Residency/Gilgdoony.java
+++ src/Elements/Residency/Gilgdoony.java
@@ -4,16 +4,16 @@ import Elements.Building;
 
 public class Gilgdoony extends Building {
     private int floors = 1;
-    private int units = 1;
+    private int unit = 1;
     private int score;
     public Gilgdoony(int floors, int units) {
 
         this.floors = floors;
-        this.units  = units;
+        this.unit = units;
     }
     public int getPopulation(){
 
-        return (floors *units) * 5;
+        return (floors * unit) * 5;
     }
 
     public int costToAddHome(int floor, int unit){
@@ -22,30 +22,44 @@ public class Gilgdoony extends Building {
     public int getUpgradeCost(String command) {
         switch (command) {
             case "floor":
-                return costToUpgradeHomeFloors( floors);
+                return costToUpgradeHomeFloors();
             case "unit":
-                return costToUpgradeHomeUnit( units);
+                return costToUpgradeHomeUnit();
             case "floor unit":
-                return costToUpgradeHomeFloorUnit( floors, units);
+                return costToUpgradeHomeFloorUnit();
         }
-        return 0 ;
+        return -1;
     }
-    private int costToUpgradeHomeFloors(int floors) {
+
+    private void addFloor(){
+        ++floors;
+    }
+    private void addUnit() {
+        ++unit;
+    }
+    private int costToUpgradeHomeFloors() {
         int a ;
-        a  = floors + (1 * units );
-        a -= (floors *units);
-        return (a * 500 + floors*300) ;
+        a  = (floors +1) * (unit);
+        a -= (floors * unit);
+        return (a * 500 + 300) ;
     }
-    private int  costToUpgradeHomeUnit(int units){
+    private int  costToUpgradeHomeUnit(){
     int a ;
-    a = floors * (units + 1);
-    a -= floors * units;
+    a = floors * (unit + 1);
+    a -= floors * unit;
     return ((a * 500));
     }
-    private int  costToUpgradeHomeFloorUnit(int floors, int units){
+
+    private int  costToUpgradeHomeFloorUnit(){
         int a ;
-        a = (floors + 1) * (units + 1);
-        a -= (floors * units);
-        return (a * 500 + floors*300);
+        a = (floors + 1) * (unit + 1);
+        a -= (floors * unit);
+        return (a * 500 + 300);
+    }
+
+    public boolean upgrade(){
+        addFloor();
+        addUnit();
+        return true;
     }
 }
