5dc7f245851a59f6725de1067a2a9d5ffe647b29
Shervin <jahanshervin@yahoo.com>
2018-10-25 16:39:46 +0330

    Updated Person class

--- src/Elements/Army.java
+++ src/Elements/Army.java
@@ -1,22 +1,44 @@
 package Elements;
 
 
-public class Army extends GilgArmy{
+import java.util.ArrayList;
 
+public class Army extends GilgArmy {
+
+    public static final int peopleNeededToBuild = 100;
+    public static final int peopleNeededToUpgrade = 10;
+    public static final int costToBuild = 15000;
+    public static final int costToUpgrade = 20000;
     private int attackLevel = 1;
+    private ArrayList<Person> people = new ArrayList<>();
+    private int armyWorking = 1;
 
-    public int getAttackLevel(){
+    public int getAttackLevel() {
         return attackLevel;
     }
 
-    Person[] peopleWorkingInArmy ;
     @Override
-    void upgrade() {
-
+    public void upgrade() {
+        attackLevel++;
     }
-    public static int score(Person p[],int n){
-        for (Person x :p) {
-            x.getDaysWorked()
+
+    public void addWorker(Person p) {
+        people.add(p);
+    }
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+
+    public int additionToLife() {
+        return armyWorking++;
+    }
+
+    public int calculateScore() {
+        int i, score = 1;
+        for (i = 0; i < armyWorking; i++) {
+            score = score * 10;
         }
+        return score;
     }
 }
